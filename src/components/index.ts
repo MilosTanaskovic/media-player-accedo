d6a97a71072824231e7c9c9561abf8cc21247463
MilosTanaskovic <milostanaskovic.dev.data@gmail.com>
2024-03-11 23:24:40 +0100

    setup media component and fullfil with dinamic media data...

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,4 @@
 export { default as Header } from "./header/Header";
-export { default as Video } from "./video/Video";
+export { default as Media } from "./media/Media";
 export { default as Player } from "./player/Player";
 export { default as Footer } from "./footer/Footer";
--- src/components/media/styles/media.module.scss.d.ts
+++ src/components/media/styles/media.module.scss.d.ts
@@ -1,4 +1,4 @@
-export const video__container: string;
+export const media__container: string;
 export const img: string;
 export const h2: string;
 export const h3: string;
