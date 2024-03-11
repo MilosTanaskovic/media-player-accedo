aaed3711aaa5944b630d270089cde78e8938a7bf
MilosTanaskovic <milostanaskovic.dev.data@gmail.com>
2024-03-11 14:45:46 +0100

    create and setup atomic design/folder structure and methodology...

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+interface FooterProps {}
+
+// eslint-disable-next-line no-empty-pattern
+const Footer: React.FC<FooterProps> = ({}) => {
+  return (
+    <footer>Footer</footer>
+  )
+}
+
+export default Footer
---
+++ src/components/player/Player.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+interface PlayerProps {}
+
+// eslint-disable-next-line no-empty-pattern
+const Player: React.FC<PlayerProps> = ({}) => {
+  return (
+    <section>Player container</section>
+  )
+}
+
+export default Player
